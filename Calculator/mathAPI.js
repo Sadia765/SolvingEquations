e0a247a27a4555a3303ea926706d2f815149386e
Sadia Ansari <sadia007@MacBook-Pro.local>
2020-08-10 08:56:18 -0500

    hard coded button using fetch and then

--- Calculator/mathAPI.js
+++ Calculator/mathAPI.js
@@ -1,14 +1,14 @@
 console.log('script loaded');
 
 window.onload = function(){
-    const buttonQuad = document.getElementById('mathExternalAPI');
-    buttonQuad.addEventListener('click', linkToAPI);
+    const buttonExternalAPI = document.getElementById('hardCodedMathExternalAPI');
+    buttonExternalAPI.addEventListener('click', linkToAPI);
 }
 
 function linkToAPI(e){
     console.log(e);
     console.log('go to external API now!');
-    const externalAPIAnswer = document.getElementById('mathAPIAnswer');
+    const externalAPIAnswer = document.getElementById('hardCodedmathAPIAnswer');
     fetch('http://api.mathjs.org/v4/?expr=2*(7-3)')
     .then((response) => {
        return response.json();
